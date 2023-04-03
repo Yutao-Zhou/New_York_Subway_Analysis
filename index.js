0d08a009f887779470f0717e03415b5079c7549b
Yutao-Zhou <13520759678@163.com>
2023-04-03 17:46:16 -0400

    Demo

--- index.js
+++ index.js
@@ -1,27 +1,132 @@
-// Initialize and add the map
-let map;
+const stationmap = {
+  "96th St": {
+    center: { lat: 40.78567199998607, lng: -73.9510700015425 },
+    enter: 2714856,
+    exit: 8405837,
+  },
+  "77th St": {
+    center: { lat: 40.77362000074615, lng: -73.95987399886047 },
+    enter: 8405837,
+    exit: 7845870,
+  },
+};
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
+  //Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+  // Create the map.
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 40.75, lng: -73.95 },
+    mapTypeId: "roadmap",
   });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+  // add transitLayer
+  const transitLayer = new google.maps.TransitLayer();
+  transitLayer.setMap(map);
+  // Construct the circle for each value in citymap.
+  // Note: We scale the area of the circle based on the population.
+  for (const station in stationmap) {
+    // Add the circle for this station to the map.
+    const enterCircle = new google.maps.Circle({
+      strokeColor: "red",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "red",
+      fillOpacity: 0.05,
+      map,
+      center: stationmap[station].center,
+      radius: Math.sqrt(stationmap[station].enter) * 0.1,
+    });
+    const exitCircle = new google.maps.Circle({
+      strokeColor: "blue",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "blue",
+      fillOpacity: 0.05,
+      map,
+      center: stationmap[station].center,
+      radius: Math.sqrt(stationmap[station].exit) * 0.1,
+    });
+  }
 }
 
 initMap();
+
+
+// Pie chart
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawPieChart);
+
+// Draw the chart and set the chart values
+function drawPieChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Work', 8],
+  ['Eat', 2],
+  ['TV', 4],
+  ['Gym', 2],
+  ['Sleep', 8]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'My Average Day', 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+
+// Bar chart
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawStacked);
+
+function drawStacked() {
+      var data = google.visualization.arrayToDataTable([
+        ['City', '2010 Population', '2000 Population'],
+        ['New York City, NY', 8175000, 8008000],
+        ['Los Angeles, CA', 3792000, 3694000],
+        ['Chicago, IL', 2695000, 2896000],
+        ['Houston, TX', 2099000, 1953000],
+        ['Philadelphia, PA', 1526000, 1517000]
+      ]);
+
+      var options = {
+        title: 'Population of Largest U.S. Cities',
+        chartArea: {width: '50%'},
+        isStacked: true,
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'City'
+        }
+      };
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
+// Curve line
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawLineChart);
+
+function drawLineChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Sales', 'Expenses'],
+    ['2004',  1000,      400],
+    ['2005',  1170,      460],
+    ['2006',  660,       1120],
+    ['2007',  1030,      540]
+  ]);
+
+  var options = {
+    title: 'Company Performance',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
